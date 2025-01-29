51b23df41ffa932506ecb3326c785582108767bc
Natalia Cano <ncano1025@gmail.com>
2025-01-28 19:05:13 -0500

    Update to intake functions and code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,5 @@
 #include "main.h"
 
-// Chassis instantiation
-std::shared_ptr<ChassisController> drive = 
-	ChassisControllerBuilder()
-		.withMotors(
-			{-7, -8, -9},
-			{4, 5, 6}
-			)
-		.withDimensions(AbstractMotor::gearset::blue, {{3.25, 10.5}, imev5BlueTPR})
-		.build();
-
-// Device instantiation
-Motor intake(19);
-pros::ADIDigitalOut piston = pros::ADIDigitalOut('A');
-
-void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
-}
-
 /**
  * A callback function for LLEMU's center button.
  *
@@ -37,6 +16,27 @@ void on_center_button() {
 	}
 }
 
+void initialize() {
+	pros::lcd::initialize();
+	pros::lcd::set_text(1, "Hello PROS User!");
+
+	pros::lcd::register_btn1_cb(on_center_button);
+}
+
+// Chassis instantiation
+std::shared_ptr<ChassisController> drive = 
+	ChassisControllerBuilder()
+		.withMotors(
+			{-7, -8, -9},
+			{4, 5, 6}
+			)
+		.withDimensions(AbstractMotor::gearset::blue, {{3.25_in, 10.5_in}, imev5BlueTPR * (600.0 / 450.0)}) // 0.75 = 8_in, 1.0 = 11.5_in, 1.333 = 14_in (SHOULD BE 0.75)
+		.build();
+
+// Device instantiation
+Motor intake(19);
+pros::ADIDigitalOut piston('A');
+
 void disabled() {}
 
 void competition_initialize() {}
@@ -46,7 +46,7 @@ void autonomous() {}
 void opcontrol() {
 
 	bool toggle = false, latch = false;
-
+	float leftY, rightY;
 	Controller controller;
 
 	while (true) {
@@ -55,25 +55,34 @@ void opcontrol() {
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
 
-		drive->getModel()->tank(
-			controller.getAnalog(ControllerAnalog::leftY),
-			controller.getAnalog(ControllerAnalog::rightY));
+		leftY = controller.getAnalog(ControllerAnalog::leftY);
+		rightY = controller.getAnalog(ControllerAnalog::rightY);
+
+		if((abs(leftY) <= 0.3) || (abs(rightY) <= 0.3)) {
+			drive->getModel()->tank(
+				leftY * 0.5,
+				rightY * 0.5,
+				0.05);
+		}
+		else {
+			drive->getModel()->tank(
+				leftY,
+				rightY);
+		}
 
 		// intake/indexer
 		if(controller.getDigital(ControllerDigital::R1))
-			intake.moveVoltage(12000);
+			intake.moveVelocity(170);
 		else if(controller.getDigital(ControllerDigital::R2)) // reverse (in the event the hook gets caught)
-			intake.moveVoltage(-12000);
+			intake.moveVelocity(-170);
 		else 
 			intake.moveVelocity(0);
 
 		// clamp toggle
-		if (toggle){
+		if (toggle)
 			piston.set_value(true); // turns clamp solenoid on
-		}
-		else {
+		else
 			piston.set_value(false); // turns clamp solenoid off
-		}
 
 		if (controller.getDigital(ControllerDigital::L1)) {
 			if(!latch){ // if latch is false, flip toggle one time and set latch to true
@@ -81,9 +90,8 @@ void opcontrol() {
 				latch = true;
 			}
 		}
-		else {
+		else
 			latch = false; //once button is released then release the latch too
-		}
 
 		pros::delay(20); // Run for 20 ms then update
 	}
